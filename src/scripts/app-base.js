e6a6505d0fd39c62e2d0af61872345f78308e330
Yakima-Teng <cleveryun@163.com>
2016-09-18 09:00:40 +0800

    纠正文章日期中月份与实际值相差一个月的bug js中月份是从0开始的，0~11对应1~12月

--- src/scripts/app-base.js
+++ src/scripts/app-base.js
@@ -132,7 +132,7 @@ angular.module('app-base', ['ngSanitize'])
         return 'Amazing Date'
       }
       var year = date.getFullYear()
-      var month = date.getMonth() < 9 ? '0' + date.getMonth() : '' + date.getMonth()
+      var month = date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : '' + (date.getMonth() + 1)
       var day = date.getDate() < 10 ? '0' + date.getDate() : '' + date.getDate()
       var hour = date.getHours() < 10 ? '0' + date.getHours() : '' + date.getHours()
       var minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : '' + date.getMinutes()
